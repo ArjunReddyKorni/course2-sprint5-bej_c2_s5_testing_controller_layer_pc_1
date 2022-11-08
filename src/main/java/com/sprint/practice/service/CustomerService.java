194c06b72d2b7087d58193321e000807c79cbcb6
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-11-08 08:56:21 +0530

    Completed

---
+++ src/main/java/com/sprint/practice/service/CustomerService.java
@@ -0,0 +1,15 @@
+package com.sprint.practice.service;
+
+import com.sprint.practice.domain.Customer;
+import com.sprint.practice.exceptions.CustomerNotFoundException;
+import com.sprint.practice.exceptions.ProductNotFoundException;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    Customer addCustomer(Customer customer);
+    List<Customer> getAllCustomer() throws CustomerNotFoundException;
+    boolean deleteCustomer(int customerId) throws CustomerNotFoundException;
+    List<Customer> getAllProductsByProductName(String productName) throws ProductNotFoundException;
+}
